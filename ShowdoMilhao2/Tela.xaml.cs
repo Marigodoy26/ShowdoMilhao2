7b6eea49ba59651a3f75ec4a2d94310f76cd90a7
Mari <mariannasantiagogodoy58@gmail.com>
2024-10-08 10:53:35 -0300

    FIM

--- ShowdoMilhao2/Tela.xaml.cs
+++ ShowdoMilhao2/Tela.xaml.cs
@@ -2,50 +2,67 @@ namespace ShowdoMilhao2;
 
 public partial class Tela : ContentPage
 {
-	Gerenciador gerenciador;
-	public Tela()
-	{
-		InitializeComponent();
-		gerenciador= new Gerenciador (labelPergunta, btResp1, btResp2, btResp3, btResp4, btResp5, labelPontuacao, labelNivel);
-	}
+    Gerenciador gerenciador;
+    public Tela()
+    {
+        InitializeComponent();
+        gerenciador = new Gerenciador(labelPergunta, btResp1, btResp2, btResp3, btResp4, btResp5, labelPontuacao, labelNivel);
+    }
 
-	  void ButtonA(object sender, EventArgs args)
-        {
-            gerenciador.VerificarCerta(1);
-        }
-     void ButtonB(object sender, EventArgs args)
-        {
-            gerenciador.VerificarCerta(2);
-        }
+    int Pula = 0;
 
-		void ButtonC(object sender, EventArgs args)
-        {
-            gerenciador.VerificarCerta(3);
-        }
+    void ButtonA(object sender, EventArgs args)
+    {
+        gerenciador.VerificarCerta(1);
+    }
+    void ButtonB(object sender, EventArgs args)
+    {
+        gerenciador.VerificarCerta(2);
+    }
 
-		void ButtonD(object sender, EventArgs args)
-        {
-            gerenciador.VerificarCerta(4);
-        }
+    void ButtonC(object sender, EventArgs args)
+    {
+        gerenciador.VerificarCerta(3);
+    }
 
-		void ButtonE(object sender, EventArgs args)
-        {
-            gerenciador.VerificarCerta(5);
-        }
+    void ButtonD(object sender, EventArgs args)
+    {
+        gerenciador.VerificarCerta(4);
+    }
+
+    void ButtonE(object sender, EventArgs args)
+    {
+        gerenciador.VerificarCerta(5);
+    }
 
     void OnAjudaRetirarClicked(Object S, EventArgs e)
-        {
-            var ajuda=new RetiraErrada();
-            ajuda.ConfiguraDesenho(btResp1, btResp2, btResp3, btResp4, btResp5);
-            ajuda.RealizaAjuda(gerenciador.GetQuestaoCorrente());
-            (S as Button).IsVisible=false;
+    {
+        var ajuda = new RetiraErrada();
+        ajuda.ConfiguraDesenho(btResp1, btResp2, btResp3, btResp4, btResp5);
+        ajuda.RealizaAjuda(gerenciador.GetQuestaoCorrente());
+        (S as Button).IsVisible = false;
 
-        }
+    }
 
-        void OnAjudaPulaClicked(object s, EventArgs e)
+    void OnAjudaPulaClicked(object s, EventArgs e)
+    {
+        if (Pula == 1)
+            (s as Button).IsVisible = false;
+        else
         {
             gerenciador.ProximaQuestao();
-            (s as Button).IsVisible=false;
+            Pula++;
         }
+    }
 
+    void AjudaPulaClicked(object s, EventArgs e)
+    {
+        if (Pula == 2)
+            (s as Button).IsVisible = false;
+        else
+        {
+            gerenciador.ProximaQuestao();
+            Pula++;
+        }
+    }
 }
